bc57d8c6e3255d05b177bc33e48677b5ff5fb92c
suphafang <suphachai.fang@gmail.com>
2023-04-17 20:49:16 +0700

    update: create signin page

--- app/src/pages/signin.tsx
+++ app/src/pages/signin.tsx
@@ -1,14 +1,26 @@
-import { Button } from "@nextui-org/react";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 
 const SignInPage = () => {
+  const router = useRouter();
+
+  const onSingin = useCallback(async () => {
+    router.push('/');
+  }, []);
+
   return (
-    <>
-      <main className="min-h-full grid place-items-center">
-        <div>
-          <Button>ดำเนินการต่อด้วย Google</Button>
-        </div>
-      </main>
-    </>
+    <div className="h-full bg-orange-500 relative">
+      <div className="p-4 py-48">
+        <h1 className="text-4xl text-white font-semibold text-center">FastFood</h1>
+        <p className="text-white text-center font-light">Fast-to-get your food</p>
+      </div>
+      <div className="absolute inset-x-0 bottom-8 p-4">
+        <button className="bg-white p-3 px-6 rounded-full w-full flex items-center" onClick={onSingin}>
+          <img src="google.svg" alt="" className="h-5" />
+          <span className="text-center w-full">ดำเนินการต่อด้วย Google</span>
+        </button>
+      </div>
+    </div>
   );
 }
 
